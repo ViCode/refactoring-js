996e6442c6d92c2107ede2b5d0da415981306153
vzeinstra <zeinstra.victor@gmail.com>
2018-01-19 15:14:00 +0100

    chore: set initial code

---
+++ dist/routes/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.routeIndex = routeIndex;
+function routeIndex(req, res) {
+    res.render('test CHANEL');
+}
