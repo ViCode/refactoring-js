c73d5b58d7ed5cf45b1dd08ab1e618a8b358ff9e
vzeinstra <zeinstra.victor@gmail.com>
2018-01-20 16:57:10 +0100

    feat(seo): add specific title and description to pages

--- src/routes/products/index.js
+++ src/routes/products/index.js
@@ -9,7 +9,9 @@ router.get('/list', isAuth, (req, res) => {
     .then(
       (rows) => res.render('list', {
         products: rows,
-        buttonViewMore: req.t('buttonViewMore')
+        buttonViewMore: req.t('buttonViewMore'),
+        title: req.t('productListTitle'),
+        desc: req.t('productListDesc')
       }),
       (err) => console.error(err)
     );
@@ -20,7 +22,9 @@ router.get('/view/:id', isAuth, (req, res) => {
     productDbo.getItem(id).then(
       (row) => res.render('view', {
         product: row,
-        buttonBuy: req.t('buttonBuy')
+        buttonBuy: req.t('buttonBuy'),
+        title: req.t('productViewTitle', {name: row.name}),
+        desc: req.t('productViewDesc', {name: row.name})
       }),
       (err) => console.error(err)
     );
