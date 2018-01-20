513aa64604a3a522155649b1fd7523c410fb931b
vzeinstra <zeinstra.victor@gmail.com>
2018-01-20 15:04:52 +0100

    refactor(translation): move json loader function to lib

--- src/app.js
+++ src/app.js
@@ -5,10 +5,9 @@ import bodyParser   from 'body-parser';
 import http         from 'http';
 import expressTranslate from 'express-translate';
 import locale from 'locale';
-import {
-  readdirSync,
-  readFileSync
-} from 'fs';
+import {readdirSync} from 'fs';
+
+import {readJsonFileSync} from './common/lib/json-loader';
 
 var app = express();
 const server = http.createServer(app);
@@ -19,12 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-let readJsonFileSync = (filepath, encoding) => {
-    encoding = encoding||'utf8';
-    var file = readFileSync(filepath, encoding);
-    return JSON.parse(file);
-}
-
+// Set translations
 app.use(locale(["en", "fr"], "en"));
 let translator = new expressTranslate();
 let translations = readJsonFileSync(__dirname + '/common/config/translate.json', 'utf8');
@@ -33,20 +27,19 @@ Object.keys(translations).forEach((key) => {
 });
 app.use(translator.middleware());
 
+// Set twig views
 app.set("view engine","twig");
 app.use(express.static(__dirname + '/common/styles'));
 app.use(express.static(__dirname + '/common/img'));
 app.set("views", __dirname + '/common/views');
 
-// reading all folders in 'routes' folder in order to set them as new node routes
+// Set routes
 readdirSync(__dirname + '/routes')
 .map(name => {
   let router = require('./routes/' + name).default;
   app.set("views", __dirname + '/routes/' + name + '/views');
   app.use('/' + name, router);
 });
-
-
 
 app.close = function() {
     server.close();
